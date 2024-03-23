331cb1a6163a5ef856a5123e95a790bdcf2e83ab
sunnyblasters <sunnyblasters@ymail.com>
2024-03-23 18:40:09 +0500

    my first guess number app

---
+++ app.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node 
+
+import inquirer  from "inquirer";
+
+let randomNumber = Math.floor(Math.random()*10)
+
+let myGuess = await inquirer.prompt([
+
+{
+
+    name:'guessNumber',
+    type : 'number',
+    message : 'Please enter your guess number',
+
+}
+
+
+])
+if (myGuess.guessNumber === randomNumber){
+
+    console.log('Yor are a Genius')
+
+}else {
+
+    console.log('Game over Please try again');
+    
+}
+
+
+console.log(randomNumber);
